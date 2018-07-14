90409369b46725698a25e0ea8bc8e50493737edb
Oleg <pora93@gmail.com>
2018-07-15 00:46:52 +0300

    Continue testing

---
+++ TestBookNinjectApp.Tests/UnitTest2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TestBookNinjectApp.Models;
+
+namespace TestBookNinjectApp.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products =
+        {
+            new Product { Name = "Kayak", Category = "Watersports", Price = 275M },
+            new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
+            new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M },
+            new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M }
+        };
+        [TestMethod]
+        public void Sum_Product_Correctly()
+        {
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
+
+            var result = target.ValueProducts(products);
+
+            Assert.AreEqual(products.Sum(e => e.Price), result);
+        }
+    }
+}
