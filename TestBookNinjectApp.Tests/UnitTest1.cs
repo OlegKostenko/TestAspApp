2960a66517d09d530a6f74e04bda5b268d33e88a
Oleg <pora93@gmail.com>
2018-07-14 23:56:53 +0300

    Add Tets Methods

--- TestBookNinjectApp.Tests/UnitTest1.cs
+++ TestBookNinjectApp.Tests/UnitTest1.cs
@@ -12,12 +12,42 @@ namespace TestBookNinjectApp.Tests
             return new MinimumDiscountHelper();
         }
         [TestMethod]
-        public void TestMethod1()
+        public void Discount_Above_100()
         {
             IDiscountHelper target = getTestObject();
             decimal total = 200;
             var discountTotal = target.ApplyDiscount(total);
             Assert.AreEqual(total * 0.9M, discountTotal);
+        }
+        [TestMethod]
+        public void Discount_Between_10_And_100()
+        {
+            IDiscountHelper target = getTestObject();
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundretDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+            Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95, HundretDollarDiscount, "$100 discount is wrong");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 discount is wrong");
+        }
+        [TestMethod]
+        public void Discount_Less_Than_10()
+        {
+            IDiscountHelper target = getTestObject();
+            decimal discount5 = target.ApplyDiscount(5);
+            decimal discount0 = target.ApplyDiscount(0);
+
+            Assert.AreEqual(5, discount5);
+            Assert.AreEqual(0, discount0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Discount_Negative_Total()
+        {
+            IDiscountHelper target = getTestObject();
+
+            target.ApplyDiscount(-1);
         }
     }
 }
